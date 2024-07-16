9020362111e3456330eb4e5b7d53f0f904345b0c
michIsMuch69 <115632651+michIsMuch69@users.noreply.github.com>
2024-07-16 11:08:52 +0200

    added includes.h file

--- headers/includes.h
+++ headers/includes.h
@@ -6,13 +6,14 @@
 /*   By: jean-micheldusserre <jean-micheldusserr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 11:01:37 by jean-michel       #+#    #+#             */
-/*   Updated: 2024/07/16 11:01:59 by jean-michel      ###   ########.fr       */
+/*   Updated: 2024/07/16 11:05:57 by jean-michel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INCLUDES_H
 # define INCLUDES_H
 
+# include "builtins.h"
 # include "libft.h"
 # include "struct.h"
 # include <stdio.h>
